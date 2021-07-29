d9e794b732f9e498f209bdc9a081d09050c50a51
codeniu <codeniu@163.com>
2021-07-29 16:11:05 +0800

    feat: react 小书第二阶段学习

--- React/reaact-naive-book/hello-react/src/components/Comment/CommentInput.jsx
+++ React/reaact-naive-book/hello-react/src/components/Comment/CommentInput.jsx
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class CommentInput extends Component {
+  static propTypes = {
+    onSubmit: PropTypes.func,
+  }
+
   constructor() {
     super()
     this.state = {
@@ -9,10 +14,26 @@ class CommentInput extends Component {
     }
   }
 
+  componentWillMount() {
+    this._loadUsername()
+  }
+
+  componentDidMount() {
+    this.textarea.focus()
+  }
+
+  _saveUsername(username) {
+    window.localStorage.setItem('username', username)
+  }
+
+  _loadUsername() {
+    this.setState({ username: window.localStorage.getItem('username') || '' })
+  }
+
   onHandlePublish(e) {
     if (this.props.onSubmit) {
       const { username, content } = this.state
-      this.props.onSubmit({ username, content })
+      this.props.onSubmit({ username, content, createdTime: +new Date() })
     }
     this.setState({ content: '' })
   }
@@ -29,6 +50,9 @@ class CommentInput extends Component {
     })
   }
 
+  handleUsernameBlur(e) {
+    this._saveUsername(e.target.value)
+  }
   render() {
     return (
       <div className="comment-input">
@@ -37,6 +61,7 @@ class CommentInput extends Component {
           <div className="comment-field-input">
             <input
               value={this.state.username}
+              onBlur={this.handleUsernameBlur.bind(this)}
               onChange={this.handleUsernameChange.bind(this)}
             />
           </div>
@@ -45,6 +70,7 @@ class CommentInput extends Component {
           <span className="comment-field-name">评论内容：</span>
           <div className="comment-field-input">
             <textarea
+              ref={textarea => (this.textarea = textarea)}
               value={this.state.content}
               onChange={this.handleContentChange.bind(this)}
             />
