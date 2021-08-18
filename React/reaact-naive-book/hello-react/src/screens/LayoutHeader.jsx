b88f02e3c65c3beecfe2afbf240d64937c859c7b
codeniu <codeniu@163.com>
2021-08-18 15:51:28 +0800

    feat: react-redux学习

--- React/reaact-naive-book/hello-react/src/screens/LayoutHeader.jsx
+++ React/reaact-naive-book/hello-react/src/screens/LayoutHeader.jsx
@@ -1,34 +1,25 @@
 import React, { Component } from 'react'
-import '../styles/layout.css'
 import PropTypes from 'prop-types'
+import { connect } from './react-redux'
+import '../styles/layout.css'
 
 class Header extends Component {
-  static contextTypes = {
-    store: PropTypes.object
-  }
-
-  constructor () {
-    super()
-    this.state = { themeColor: '' }
-  }
-
-  componentWillMount() {
-    const { store } = this.context
-    this._updateThemeColor()
-    store.subscribe(() => this._updateThemeColor())
-  }
-
-  _updateThemeColor () {
-    const { store } = this.context
-    const state = store.getState()
-    this.setState({ themeColor: state.themeColor })
+  static propTypes = {
+    themeColor: PropTypes.string
   }
 
   render () {
     return (
-      <h1 style={{ color: this.state.themeColor }}>React.js 小书</h1>
+      <h1 style={{ color: this.props.themeColor }}>React.js 小书</h1>
     )
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    themeColor: state.themeColor
+  }
+}
+Header = connect(mapStateToProps)(Header)
 
 export default Header
