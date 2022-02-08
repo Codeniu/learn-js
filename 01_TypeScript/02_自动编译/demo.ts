896bf4a683df5f978471f76c6f3a50759bbb5162
codeniu <jingxiangniu@gmail.com>
2022-02-08 09:19:34 +0800

    格式化代码

--- 01_TypeScript/02_自动编译/demo.ts
+++ 01_TypeScript/02_自动编译/demo.ts
@@ -1,20 +1,19 @@
-class Person{
+class Person {
   firstName = ''
   lastName = ''
   fullName = ''
-  constructor(firstName:string,lastName:string){
+  constructor(firstName: string, lastName: string) {
     this.firstName = firstName
     this.lastName = lastName
-    this.fullName = this.firstName+'_'+this.lastName
+    this.fullName = this.firstName + '_' + this.lastName
   }
 
-  showName(){
+  showName() {
     return this.fullName
   }
 }
 
-const xiaohong = new Person('xiao','hong')
+const xiaohong = new Person('xiao', 'hong')
 console.log(xiaohong.showName())
 
-
-// 通过查看编译后的ja文件可以看到， TypeScript 中的类只是一个语法糖，本质商还是js的函数
+// 通过查看编译后的js文件可以看到， TypeScript 中的类只是一个语法糖，本质上还是js的函数
